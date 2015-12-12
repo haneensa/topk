37f27fb4a68fc7f1660fe8e5483b6eb61732c97b
Haneen Mohammed <hamohammed.sa@gmail.com>
2015-12-12 15:38:56 +0300

    working

--- topk_int.cpp
+++ topk_int.cpp
@@ -21,31 +21,37 @@ int main(int argc, const char * argv[]) {
     int k = 10;
     long long mySum;
     unsigned int keys[21];
+    // unsigned int *keys = (unsigned int*)calloc(SIZE,sizeof(unsigned int));
     string line;
     
     /****** processing **********/
     const char* FILENAM = "/Users/haneen/Downloads/kaust/CS245/project2/binary_data_int.dat";
-   // ifstream ss(FILENAM,ios::binary);
-    FILE* in = fopen(FILENAM, "rb");
-    unsigned int *buf = (unsigned int*)mmap(
-                                            0,
-                                            SIZE * sizeof(unsigned int),
-                                            PROT_READ,
-                                            MAP_FILE | MAP_PRIVATE,
-                                            fileno(in),
-                                            0
-                                            );
+    ifstream ss(FILENAM,ios::binary);
+    /*FILE* in = fopen(FILENAM, "rb");
+    unsigned int *keys = (unsigned int*)mmap(
+                                             0,
+                                             SIZE * sizeof(unsigned int),
+                                             PROT_READ,
+                                             MAP_FILE | MAP_PRIVATE,
+                                             fileno(in),
+                                             0
+                                             );
     fclose(in);
+    */
+    //   ss.read((char*)keys, SIZE*sizeof(unsigned int));
     
     for (int j = 0; j < 50000000; j++) {
-        memcpy(keys, buf+21*j, 21 * sizeof(unsigned int));
+        //    memcpy(keys, buf+j*21, 21 * sizeof(unsigned int));
+        
         mySum = 0;
-       //  ss.read((char*)&keys, 21*sizeof(unsigned int));
+        ss.read((char*)&keys, 21*sizeof(unsigned int));
         
         // get the specified columns (i, j)
         // sum them up
-        for (int i = 1; i < 21; i++)
+        for (int i = 1; i < 3; i++) {
+          //  mySum += keys[i+j*21];
             mySum += keys[i];
+        }
         
         // add them to the min heap
         if (mypq.size() < k ) {
@@ -64,7 +70,7 @@ int main(int argc, const char * argv[]) {
     }
     
     // delete[] memblock;
-    munmap(buf, SIZE * sizeof(unsigned int));
+    //munmap(keys, SIZE * sizeof(unsigned int));
     
     
     return 0;
